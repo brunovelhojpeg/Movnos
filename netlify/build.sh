543786844c9162fb00f2a3c7f8358b844d26e978
Bruno  Azevedo <brunoazevedo@MacBook-Air-de-Bruno-2.local>
2026-02-08 18:18:46 -0300

    Use Flutter 3.38.9 on Netlify build

--- netlify/build.sh
+++ netlify/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-FLUTTER_VERSION="3.22.3"
+FLUTTER_VERSION="3.38.9"
 
-git clone --depth 1 --branch "$FLUTTER_VERSION" https://github.com/flutter/flutter.git flutter_sdk
-export PATH="$PWD/flutter_sdk/bin:$PATH"
+git clone --depth 1 --branch "$FLUTTER_VERSION" https://github.com/flutter/flutter.git
+export PATH="$PWD/flutter/bin:$PATH"
 
 flutter --version
 flutter config --enable-web
